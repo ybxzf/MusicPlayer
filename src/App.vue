ef8b451c7cc5488022519399528d923a1293cb89
ybxzf <42826051+ybxzf@users.noreply.github.com>
2023-07-12 15:41:16 +0800

    Create App.vue

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <main>
+    <music />
+    <!-- <test /> -->
+  </main>
+</template>
+
+<script setup>
+import music from "@/views/homepage/music.vue";
+import test from "@/components/test.vue";
+</script>
+
+<style scoped></style>
